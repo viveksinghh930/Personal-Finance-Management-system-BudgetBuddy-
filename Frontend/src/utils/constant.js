1baba7adf715690744c509a8586ef77419c5e7c6
Vivek Singh Tomar <tomarvivek669@gmail.com>
2026-02-24 14:47:09 +0530

    cleanup: remove unused env variables

--- Frontend/src/utils/constant.js
+++ Frontend/src/utils/constant.js
@@ -1,3 +1,1 @@
-export const USER_API_END_POINT = import.meta.env.VITE_USER_API_END_POINT;
-export const INCOME_API_END_POINT = import.meta.env.VITE_INCOME_API_END_POINT;
 export const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID;
